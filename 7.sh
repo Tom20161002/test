397b0f2a56a72b3d6526ae00856f89f0f1066a0f
sunnyliu <1322970235@qq.com>
2019-02-01 22:14:31 +0800

    Update 7.sh
    #计算系统进程占用内容大小和，1.获取内存大小top ps  2.进行统计

--- 7.sh
+++ 7.sh
@@ -1,1 +1,10 @@
 #!/usr/bin/sh
+#计算系统进程占用内容大小和，1.获取内存大小top ps  2.进行统计
+Mem=$(ps aux|awk '{print $6}'|grep -v "RSS")
+num=0
+for i in $Mem
+do
+  num=$[($i+$num)]
+done
+  num=$[$num/1024]
+  echo ${num}M
